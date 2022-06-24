afcd912b7adb875622a30ac158967d0d66c70e9d
Tiara Mendiola <dev.tiaram@gmail.com>
2022-06-24 13:10:44 -0500

    Reorganized the order of functions and snips of code

--- src/index.js
+++ src/index.js
@@ -14,6 +14,144 @@ function getDayOrNight() {
 }
 /*---------------------------------------------------------------------------*/
 
+/*----------------------------------------------------------------------------------------------------------------------*/
+
+//Date format: Day, Month DD, YYYY
+function getTodaysDate(now) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let months = [
+    "January",
+    "Feburary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let day = days[now.getDay()];
+  let month = months[now.getMonth()];
+  let date = now.getDate();
+  let year = now.getFullYear();
+
+  return `${day}, ${month} ${date}, ${year}`;
+}
+
+//Time format: 12-hour period AM/PM
+function getTimeNow() {
+  let now = new Date();
+  let hour = String(now.getHours()).padStart(2, "0");
+  let minutes = String(now.getMinutes()).padStart(2, "0");
+  let seconds = String(now.getSeconds()).padStart(2, "0");
+  let meridiem = "";
+
+  if (hour < 12) {
+    meridiem = "AM";
+  } else if (hour === 12) {
+    meridiem = "PM";
+  } else if (hour === 24) {
+    hour = hour - 12;
+    meridiem = "AM";
+  } else {
+    hour = hour - 12;
+    meridiem = "PM";
+  }
+
+  if (hour === 0) {
+    hour = 12;
+  }
+  let time = `${hour}:${minutes}:${seconds} ${meridiem}`;
+  timeNow.innerHTML = `${time}`;
+
+  //Performs function for a 'live clock'
+  let t = setTimeout(function () {
+    getTimeNow();
+  }, 1000);
+}
+
+//Greeting format: Morning 12AM-11AM, Afternoon 12PM-5PM, Evening 6PM-11PM
+function getGreeting(now) {
+  let hour = now.getHours();
+  let greeting = "";
+  let image = document.querySelector("#greetingIcon");
+
+  if (hour < 12) {
+    greeting = "Good Morning";
+  } else if (hour >= 12 && hour < 18) {
+    greeting = "Good Afternoon";
+    image.setAttribute("src", `./media/afternoon.png`);
+  } else if (hour > 17 && hour < 24) {
+    greeting = "Good Evening";
+    image.setAttribute("src", `./media/evening.png`);
+  } else {
+    greeting = "Good Morning";
+    image.setAttribute("src", `./media/afternoon.pgn`);
+  }
+  return `${greeting}`;
+}
+
+//Handles event when Fahrenheit button is clicked
+function displayFahrenheit(event) {
+  event.preventDefault();
+  let fahrenheitElem = document.querySelector("#temperature");
+  fahrenheitTemp = Math.round((celsiusTemperature * 9) / 5 + 32);
+  fahrenheitElem.innerHTML = fahrenheitTemp;
+
+  celsiusBttn.classList.remove("active");
+  fahrenheitBttn.classList.add("active");
+  celsiusBttn.style.color = "rgb(154, 81, 177)";
+  fahrenheitBttn.style.color = "black";
+  fahrenheitBttn.style.fontWeight = "bold";
+  celsiusBttn.style.fontWeight = "400";
+
+  let feelsLikeFElem = document.querySelector("#feelsLike");
+  let feelsLikeF = Math.round((feelsLikeC * 9) / 5 + 32);
+  feelsLikeFElem.innerHTML = `${feelsLikeF} °F`;
+
+  let windSpeed = document.querySelector("#windSpeed");
+  let windMph = Math.round(windKmH / 1.609344);
+  windSpeed.innerHTML = `${windMph} mph`;
+}
+
+//Handles event when Celsius button is clicked
+function displayCelsius(event) {
+  event.preventDefault();
+  let celsius = document.querySelector("#temperature");
+  celsius.innerHTML = Math.round(celsiusTemperature);
+
+  celsiusBttn.classList.add("active");
+  fahrenheitBttn.classList.remove("active");
+  fahrenheitBttn.style.color = "rgb(154, 81, 177)";
+  celsiusBttn.style.color = "black";
+  celsiusBttn.style.fontWeight = "bold";
+  fahrenheitBttn.style.fontWeight = "400";
+
+  let feelsLikeCElem = document.querySelector("#feelsLike");
+  feelsLikeCElem.innerHTML = `${feelsLikeC} °C`;
+
+  let windSpeed = document.querySelector("#windSpeed");
+  windSpeed.innerHTML = `${windKmH} km/h`;
+}
+
+//----------------------------------------------------------------------------------------------------------------------//
+
+//function getHourlyForecast(){}
+
+//Handles & displays the weekly forecast weather icon images
 function updateWeatherIcon(main) {
   console.log(`main is ${main}`);
   let imgSrc = "";
@@ -37,13 +175,16 @@ function updateWeatherIcon(main) {
   return imgSrc;
 }
 
+//Formats the min temp
 function formatMinTemp(minTemp) {
   return Math.round(minTemp);
 }
+//Formats the max temp
 function formatMaxTemp(maxTemp) {
   return Math.round(maxTemp);
 }
 
+//Formats the 'dt' data to display the day & date of the Weekly Forecast
 function formatDT(timestamp) {
   let now = new Date(timestamp * 1000);
   let day = now.getDay();
@@ -53,6 +194,7 @@ function formatDT(timestamp) {
   return `${days[day]} ${date}`;
 }
 
+//Displays the weekly forecast; several function calls to format temps and display weather icon
 function displayWeeklyForecast(response) {
   let weeklyForecastElem = document.querySelector("#weekly-Forecast");
   console.log(response.data.daily);
@@ -92,6 +234,7 @@ function displayWeeklyForecast(response) {
   weeklyForecastElem.innerHTML = weeklyForecastHTML;
 }
 
+//Gets the weekly forecast, utilizes location coordinates and one call api from open weather map
 function getWeeklyForecast(response) {
   console.log(response.data);
   let lattitude = response.data.coord.lat;
@@ -102,33 +245,7 @@ function getWeeklyForecast(response) {
   axios.get(apiUrl).then(displayWeeklyForecast);
 }
 
-function displayDefaultForecast() {
-  let weeklyForecastElem = document.querySelector("#weekly-Forecast");
-  let weeklyForecastHTML = `<div class="row m-1 pb-3">`;
-  let days = ["Thurs", "Fri", "Sat", "Sun", "Mon", "Tues"];
-  days.forEach(function (day) {
-    weeklyForecastHTML =
-      weeklyForecastHTML +
-      `
-      <div class="col-sm-2 p-0">
-        <div class="card m-1 p-2 h-100 bg-card-details">
-          <div class="forecast-date p-0">${day}
-          </div>
-          <img src="./media/dustSand.png" class="forecast-icon" id="forecast-icon" alt="weekly forecast weather Icon">
-          <div>
-          <span class="forecast-max-temp">89°</span> | 
-          <span class="forecast-min-temp">79°</span>
-          </div>
-        </div>
-      </div>
-      `;
-  });
-
-  weeklyForecastHTML = weeklyForecastHTML + `</div>`;
-  weeklyForecastElem.innerHTML = weeklyForecastHTML;
-}
-
-/*------------Updating the weather Icons------------------------------------*/
+/*----------------------------------------------Updates the Current Weather Icon----------------------------------------*/
 function getAtmosphereIcon(response) {
   let description = response.data.weather[0].description;
   let weatherIconMainElem = document.querySelector("#weatherIconMain");
@@ -274,12 +391,14 @@ function getClearSkyIcon(response) {
   document.body.style.backgroundSize = "cover";
 }
 
+//Gets 'main' weather description
 function getMainWeather(response) {
   let main = response.data.weather[0].main;
   console.log(`get main weather ${main}`);
   return main;
 }
 
+//-----------------------Collection of function calls to display the Current Weather Icon------------------------//
 function updateMainWeatherIcon(response) {
   let main = getMainWeather(response);
   console.log(`update main: main is ${main}`);
@@ -302,7 +421,45 @@ function updateMainWeatherIcon(response) {
   }
 }
 
-/*-----------------------------------------------------------*/
+//Get & display the wind speed (default in km/h)
+function getWindSpeed(response) {
+  let windSpeedElem = document.querySelector("#windSpeed");
+  let wind = Math.round(response.data.wind.speed * 3.6);
+  windSpeedElem.innerHTML = `${wind} km/h`;
+  windKmH = wind;
+}
+
+//Get & display the humidity
+function getHumidity(response) {
+  let humidityElem = document.querySelector("#humidity");
+  let humidity = Math.round(response.data.main.humidity);
+  humidityElem.innerHTML = `${humidity}%`;
+}
+
+//Get & display the "feels like" temperature (defualt in Celsius)
+function getFeelsLike(response) {
+  let feelsLikeElem = document.querySelector("#feelsLike");
+  let feelsLike = Math.round(response.data.main.feels_like);
+  feelsLikeElem.innerHTML = `${feelsLike} °C`;
+  feelsLikeC = feelsLike;
+}
+
+//Get & display the temperature (default in Celsius)
+function getTemperature(response) {
+  let temperatureElem = document.querySelector("#temperature");
+  let temperature = Math.round(response.data.main.temp);
+  temperatureElem.innerHTML = `${temperature}`;
+  celsiusTemperature = response.data.main.temp;
+}
+
+//Get & display the weather forecast description
+function getWeatherDescription(response) {
+  let description = response.data.weather[0].description;
+  let descriptionElem = document.querySelector(".weatherStatus");
+  descriptionElem.innerHTML = `${description}`;
+}
+
+//Get & display the location
 function getLocation(response) {
   let locationElement = document.querySelector("#location");
   let city = response.data.name;
@@ -313,39 +470,7 @@ function getLocation(response) {
   console.log(response.data);
 }
 
-function getWeatherDescription(response) {
-  let description = response.data.weather[0].description;
-  let descriptionElem = document.querySelector(".weatherStatus");
-  descriptionElem.innerHTML = `${description}`;
-}
-
-function getTemperature(response) {
-  let temperatureElem = document.querySelector("#temperature");
-  let temperature = Math.round(response.data.main.temp);
-  temperatureElem.innerHTML = `${temperature}`;
-  celsiusTemperature = response.data.main.temp;
-}
-
-function getFeelsLike(response) {
-  let feelsLikeElem = document.querySelector("#feelsLike");
-  let feelsLike = Math.round(response.data.main.feels_like);
-  feelsLikeElem.innerHTML = `${feelsLike} °C`;
-  feelsLikeC = feelsLike;
-}
-
-function getHumidity(response) {
-  let humidityElem = document.querySelector("#humidity");
-  let humidity = Math.round(response.data.main.humidity);
-  humidityElem.innerHTML = `${humidity}%`;
-}
-
-function getWindSpeed(response) {
-  let windSpeedElem = document.querySelector("#windSpeed");
-  let wind = Math.round(response.data.wind.speed * 3.6);
-  windSpeedElem.innerHTML = `${wind} km/h`;
-  windKmH = wind;
-}
-
+//---------------------Collection of function calls to display the weather forecast----------------------------------//
 function getWeather(response) {
   getLocation(response);
   getWeatherDescription(response);
@@ -358,17 +483,18 @@ function getWeather(response) {
   getWeeklyForecast(response);
 }
 
-/*---------------------------------------------------------------------------------------------*/
-
+//Handles user's input, checks if entry is valid, then gets weather if valid or displays error message
 function searchLocation(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   let location = document.querySelector("#location");
 
-  //Condition for blank entries
+  //Conditions for blank entries
   let input = searchInput.value.trim();
   if (input) {
     location.innerHTML = `Searching city...`;
+
+    //------------------consider an invalid city search here-------------------------------//
   } else {
     location.innerHTML = null;
     alert("Please enter a city.");
@@ -387,6 +513,7 @@ function searchLocation(event) {
   document.getElementById("search-form").reset();
 }
 
+//Sets and utilizes user's location coordinates to get weather forecast
 function getPosition(position) {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
@@ -394,172 +521,58 @@ function getPosition(position) {
   let apiKey = "32002ce1ac753de34a94e79ba08a9e9b";
   let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
   let apiUrl = `${apiEndpoint}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+  //Get weather data
   axios.get(`${apiUrl}`).then(getWeather);
 }
 
+//Handles "Check my location" button action to get user's location coordinates
 function clickedCurrent() {
   navigator.geolocation.getCurrentPosition(getPosition);
 }
 
-//Date format: Day, Month DD, YYYY
-function getTodaysDate(now) {
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-
-  let months = [
-    "January",
-    "Feburary",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-
-  let day = days[now.getDay()];
-  let month = months[now.getMonth()];
-  let date = now.getDate();
-  let year = now.getFullYear();
-
-  return `${day}, ${month} ${date}, ${year}`;
+//Displays default weather based on specific city provided
+function displayDefaultWeather(city) {
+  let units = "metric";
+  let apiKey = "32002ce1ac753de34a94e79ba08a9e9b";
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=${units}`;
+  //Get weather data
+  axios.get(`${apiUrl}`).then(getWeather);
 }
 
-//Time format: 12-hour period AM/PM
-function getTimeNow() {
-  let now = new Date();
-  let hour = String(now.getHours()).padStart(2, "0");
-  let minutes = String(now.getMinutes()).padStart(2, "0");
-  let seconds = String(now.getSeconds()).padStart(2, "0");
-  let meridiem = "";
+/*----------------------------------------------------------------------------------------------*/
 
-  if (hour < 12) {
-    meridiem = "AM";
-  } else if (hour === 12) {
-    meridiem = "PM";
-  } else if (hour === 24) {
-    hour = hour - 12;
-    meridiem = "AM";
-  } else {
-    hour = hour - 12;
-    meridiem = "PM";
-  }
-
-  if (hour === 0) {
-    hour = 12;
-  }
-  let time = `${hour}:${minutes}:${seconds} ${meridiem}`;
-  timeNow.innerHTML = `${time}`;
-
-  //Performs function for a 'live clock'
-  let t = setTimeout(function () {
-    getTimeNow();
-  }, 1000);
-}
-
-//Greeting format: Morning 12AM-11AM, Afternoon 12PM-5PM, Evening 6PM-11PM
-function getGreeting(now) {
-  let hour = now.getHours();
-  let greeting = "";
-  let image = document.querySelector("#greetingIcon");
-
-  if (hour < 12) {
-    greeting = "Good Morning";
-  } else if (hour >= 12 && hour < 18) {
-    greeting = "Good Afternoon";
-    image.setAttribute("src", `./media/afternoon.png`);
-  } else if (hour > 17 && hour < 24) {
-    greeting = "Good Evening";
-    image.setAttribute("src", `./media/evening.png`);
-  } else {
-    greeting = "Good Morning";
-    image.setAttribute("src", `./media/afternoon.pgn`);
-  }
-  return `${greeting}`;
-}
-
-//Fahrenheit/Celsisu function
-
-function displayFahrenheit(event) {
-  event.preventDefault();
-  let fahrenheitElem = document.querySelector("#temperature");
-  fahrenheitTemp = Math.round((celsiusTemperature * 9) / 5 + 32);
-  fahrenheitElem.innerHTML = fahrenheitTemp;
-
-  celsiusBttn.classList.remove("active");
-  fahrenheitBttn.classList.add("active");
-  celsiusBttn.style.color = "rgb(154, 81, 177)";
-  fahrenheitBttn.style.color = "black";
-  fahrenheitBttn.style.fontWeight = "bold";
-  celsiusBttn.style.fontWeight = "400";
-
-  let feelsLikeFElem = document.querySelector("#feelsLike");
-  let feelsLikeF = Math.round((feelsLikeC * 9) / 5 + 32);
-  feelsLikeFElem.innerHTML = `${feelsLikeF} °F`;
-
-  let windSpeed = document.querySelector("#windSpeed");
-  let windMph = Math.round(windKmH / 1.609344);
-  windSpeed.innerHTML = `${windMph} mph`;
-}
-
-function displayCelsius(event) {
-  event.preventDefault();
-  let celsius = document.querySelector("#temperature");
-  celsius.innerHTML = Math.round(celsiusTemperature);
-
-  celsiusBttn.classList.add("active");
-  fahrenheitBttn.classList.remove("active");
-  fahrenheitBttn.style.color = "rgb(154, 81, 177)";
-  celsiusBttn.style.color = "black";
-  celsiusBttn.style.fontWeight = "bold";
-  fahrenheitBttn.style.fontWeight = "400";
-
-  let feelsLikeCElem = document.querySelector("#feelsLike");
-  feelsLikeCElem.innerHTML = `${feelsLikeC} °C`;
-
-  let windSpeed = document.querySelector("#windSpeed");
-  windSpeed.innerHTML = `${windKmH} km/h`;
-}
-
-//Current date
+//Display user's local date
 let now = new Date();
 let dateToday = document.querySelector("#date-today");
 dateToday.innerHTML = getTodaysDate(now);
 
-//Current time
+//Display user's local time
 let timeNow = document.querySelector("#time-now");
 getTimeNow();
 
-//Greeting based off local time
+//Greeting message displayed based off user's local time
 let greeting = document.querySelector("#greeting");
 greeting.innerHTML = getGreeting(now);
 
-//Current location button
-let currentLocationBttn = document.querySelector("#currentLocationBttn");
-currentLocationBttn.addEventListener("click", clickedCurrent);
-
-//Fahrenheit Button
+//Fahrenheit button clicked event
 let celsiusTemperature = null;
 let windKmH = null;
 let feelsLikeC = null;
 let fahrenheitBttn = document.querySelector("#fahrenheit-link");
 fahrenheitBttn.addEventListener("click", displayFahrenheit);
 
-//Celsius Button
+//Celsius button clicked event
 let celsiusBttn = document.querySelector("#celsius-link");
 celsiusBttn.addEventListener("click", displayCelsius);
 
 //Search Engine Form Submission
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchLocation);
+
+//Current location button clicked event
+let currentLocationBttn = document.querySelector("#currentLocationBttn");
+currentLocationBttn.addEventListener("click", clickedCurrent);
+
+//Default display upon loading page
+displayDefaultWeather("Guam");
